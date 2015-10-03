9217e9db4f3f84cf57b4bff2685bfc6758b13167
Joseph Smith <jcsmith1234@gmail.com>
2015-10-03 19:04:44 -0400

    Add links and fix contacts and resume pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914041257) do
+ActiveRecord::Schema.define(version: 20150917053947) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150914041257) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "website"
+    t.string   "link"
   end
 
   create_table "users", force: :cascade do |t|
